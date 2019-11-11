c5b434ff8d04f5da35bc715f8da24d4eadcc7d54
marcin najder <marcin.najder@comarch.pl>
2019-11-11 11:54:06 +0100

    using published to npm version of powerfp

--- 2019_04_06_make_a_lisp_in_typescript/src/useDevPowerfp.ts
+++ 2019_04_06_make_a_lisp_in_typescript/src/useDevPowerfp.ts
@@ -1,17 +1,37 @@
-import * as path from "path";
-initEnv();
+// to use development version of powerfp library (instead of npm version):
 
-function initEnv() {
-  const Module = require('module');
-  const modulePrefixLength = "powerfp".length;
-  const _require = Module.prototype.require;
+// 1. uncommend js code below
 
-  Module.prototype.require = function (modulePath: string) {
-    if (modulePath.startsWith("powerfp")) {
-      //modulePath = "/Volumes/data/github/powerfp/dist/cjs_es6/src/index";
-      modulePath = path.join("/Volumes/data/github/powerfp/dist/cjs_es6/src/", modulePath.substr(modulePrefixLength));
-    }
-    return _require.call(this, modulePath);
-  };
-}
+// import * as path from "path";
+// initEnv();
+
+// function initEnv() {
+//   const Module = require('module');
+//   const modulePrefixLength = "powerfp".length;
+//   const _require = Module.prototype.require;
+
+//   Module.prototype.require = function (modulePath: string) {
+//     if (modulePath.startsWith("powerfp")) {
+//       //modulePath = "/Volumes/data/github/powerfp/dist/cjs_es6/src/index";
+//       modulePath = path.join("/Volumes/data/github/powerfp/dist/cjs_es6/src/", modulePath.substr(modulePrefixLength));
+//     }
+//     return _require.call(this, modulePath);
+//   };
+// }
+
+
+// 2. add below JSON configuration to tsconfig.json inside 'compilerOptions' section
+
+// "paths": {
+//   "powerfp": [
+//     "/Volumes/data/github/powerfp/dist/cjs_es6/src/index.d.ts",
+//   ],
+//   "powerfp/*": [
+//     "/Volumes/data/github/powerfp/dist/cjs_es6/src/*",
+//   ]
+// }
+
+
+
+
 
