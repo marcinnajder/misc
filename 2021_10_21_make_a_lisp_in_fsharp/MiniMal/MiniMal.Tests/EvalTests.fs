4307638709aacf9560a25b211017a2f58c6ef70f
marcin najder <marcin.najder@comarch.pl>
2021-10-23 14:09:18 +0200

    (mal in fsharp) applyDo, applyIf

--- 2021_10_21_make_a_lisp_in_fsharp/MiniMal/MiniMal.Tests/EvalTests.fs
+++ 2021_10_21_make_a_lisp_in_fsharp/MiniMal/MiniMal.Tests/EvalTests.fs
@@ -50,3 +50,65 @@ type EvalTests() =
                 (defaultEnv None)
 
         assertMalEquals (Number(4.)) malResult
+
+
+    [<TestMethod>]
+    member this.ApplyDoTest() =
+        assertThrowsException (fun () -> applyDo [] (emptyEnv None))
+        let malResult =
+            applyDo [ Nil; MalList([ Symbol("+"); Number(1.); Number(3.) ], List) ] (defaultEnv None)
+
+        assertMalEquals (Number(4.)) malResult
+
+    [<TestMethod>]
+    member this.ApplyIfTest() =
+        assertThrowsException (fun () -> applyIf [] (emptyEnv None))
+        assertThrowsException (fun () -> applyIf [ Symbol("a") ] (emptyEnv None))
+        assertThrowsException (fun () -> applyIf [ Symbol("a"); Symbol("a"); Symbol("a"); Symbol("a") ] (emptyEnv None))
+        // falsy values
+        assertMalEquals (Number(2.)) (applyIf [ False; Number(1.); Number(2.) ] (emptyEnv None))
+        assertMalEquals (Number(2.)) (applyIf [ Nil; Number(1.); Number(2.) ] (emptyEnv None))
+        assertMalEquals Nil (applyIf [ Nil; Number(1.) ] (emptyEnv None))
+        // truthy values
+        assertMalEquals (Number(1.)) (applyIf [ True; Number(1.); Number(2.) ] (emptyEnv None))
+        assertMalEquals (Number(1.)) (applyIf [ Str(""); Number(1.); Number(2.) ] (emptyEnv None))
+        assertMalEquals (Number(1.)) (applyIf [ MalList([], Vector); Number(1.); Number(2.) ] (emptyEnv None))
+
+//         [TestMethod]
+//         public void BindFunctionArgumentsTest()
+//         {
+//             Symbol a = new Symbol("a"), b = new Symbol("b"), amp = new Symbol("&");
+//             MalType one = new Number(1), two = new Number(2), three = new Number(3);
+
+//             Assert.AreEqual(LListFrom((a, one)), EvalM.BindFunctionArguments(MalLListFrom(a), LListFrom(one)));
+//             Assert.AreEqual(null, EvalM.BindFunctionArguments(MalLListFrom(), LListFrom(one)));
+//             Assert.AreEqual(LListFrom((a, one)), EvalM.BindFunctionArguments(MalLListFrom(a), LListFrom(one, two)));
+//             Assert.ThrowsException<Exception>(() => EvalM.BindFunctionArguments(MalLListFrom(a), MalLListFrom()));
+
+//             Assert.AreEqual(
+//                 LListFrom((a, one), (b, new List(LListFrom(two, three), ListType.List))),
+//                 EvalM.BindFunctionArguments(MalLListFrom(a, amp, b), LListFrom(one, two, three)));
+//             Assert.AreEqual(
+//                 LListFrom((a, one), (b, new List(null, ListType.List))),
+//                 EvalM.BindFunctionArguments(MalLListFrom(a, amp, b), LListFrom(one)));
+
+//             Assert.ThrowsException<Exception>(() => Assert.AreEqual(
+//                 LListFrom((a, one), (b, new List(null, ListType.List))),
+//                 EvalM.BindFunctionArguments(MalLListFrom(a, amp), LListFrom(one))));
+//         }
+
+//         [TestMethod]
+//         public void ApplyFnTest()
+//         {
+//             Assert.ThrowsException<Exception>(() => EvalM.ApplyFn(MalLListFrom(), EmptyEnv()));
+//             Assert.ThrowsException<Exception>(() => EvalM.ApplyFn(MalLListFrom(new Number(1), new Number(1)), EmptyEnv()));
+//             Assert.ThrowsException<Exception>(() => EvalM.ApplyFn(MalLListFrom(MalListFrom(), new Number(1), new Number(1)), EmptyEnv()));
+
+
+//             Symbol a = new Symbol("a"), b = new Symbol("b"), plus = new Symbol("+");
+//             MalType one = new Number(1), two = new Number(2);
+
+//             var fn = (Fn)EvalM.ApplyFn(MalLListFrom(MalListFrom(a, b), MalListFrom(plus, a, b)), DefaultEnv());
+
+//             Assert.AreEqual(new Number(3), fn.Value(LListFrom(one, two)));
+//         }
--- 2021_10_21_make_a_lisp_in_fsharp/MiniMal/MiniMal/Eval.fs
+++ 2021_10_21_make_a_lisp_in_fsharp/MiniMal/MiniMal/Eval.fs
@@ -11,6 +11,8 @@ let rec eval mal (env: Env) =
         | [] -> mal
         | Symbol ("def!") :: tail -> applyDef tail env
         | Symbol ("let*") :: tail -> applyLet tail env
+        | Symbol ("do") :: tail -> applyDo tail env
+        | Symbol ("if") :: tail -> applyIf tail env
         | _ ->
             match (evalAst mal env) with
             | MalList (Fn (fn, _) :: args, _) -> fn args
@@ -47,7 +49,7 @@ and internal applyLet items env =
         |> eval expr
     | _ ->
         failwith
-            $"'def!' requires 2 arguments where the first argument must be 'symbol', but got '{joinWithSeparator items None}'"
+            $"'let*' requires 2 arguments where the first is a list of bindings and the second is a Mal expression, but got '{joinWithSeparator items None}'"
 
 // ( a 1 b 3 )
 and internal applyBindings items env =
@@ -60,3 +62,48 @@ and internal applyBindings items env =
     | _ ->
         failwith
             $"Bindings argument in let* should contain an even number of elements where even element must be a 'symbol', but got '{joinWithSeparator items None}'"
+
+// (do (...) (...) (....) )
+and internal applyDo items env =
+    match items with
+    | [ head ] -> eval head env
+    | head :: tail ->
+        eval head env |> ignore
+        applyDo tail env
+    | [] -> failwith $"'do' requires at least one argument"
+
+// (if (...) (...) (...) )
+and internal applyIf items env =
+    match items with
+    | [ if_; then_; _ ]
+    | [ if_; then_ ] ->
+        match (eval if_ env) with
+        | False
+        | Nil ->
+            match items with
+            | [ _; _; else_ ] -> eval else_ env
+            | _ -> Nil
+        | _ -> eval then_ env
+    | _ -> failwith $"'if' requires 2 or 3 arguments but got '{joinWithSeparator items None}'"
+
+
+// // (if (...) (...) (...) )
+// internal static MalType ApplyIf(LList<MalType>? items, Env env)
+//     => items switch
+//     {
+//         (var If, (var Then, (null or (_, null))) ThenElse) => Eval(If, env).Pipe(@if => @if switch
+//        {
+//            not (Nil or False) => Eval(Then, env),
+//            _ => ThenElse.Tail == null ? NilV : Eval(ThenElse.Tail.Head, env)
+//        }),
+//         _ => throw new Exception($"'if' requires 2 or 3 arguments but got '{items.JoinWithSeparator()}'")
+//     };
+
+// // (fn* (...) ...)
+// internal static MalType ApplyFn(LList<MalType>? items, Env env)
+//      => items switch
+//      {
+//          (List { Items: var ArgNames }, (var Body, null)) =>
+//             new Fn(argsValues => Eval(Body, new Env(MapM.MapFrom(BindFunctionArguments(ArgNames, argsValues)), env))),
+//          _ => throw new Exception($"'fn*' requires 2 arguments where the first one must be a 'list', but got '{items.JoinWithSeparator()}'")
+//      };
