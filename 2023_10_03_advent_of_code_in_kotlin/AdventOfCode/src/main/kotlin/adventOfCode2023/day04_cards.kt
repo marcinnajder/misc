d2b6a9f43b42ec14a848a3dab7698aca19cbc1dd
marcin najder <marcin.najder@gmail.com>
2023-12-06 20:48:51 +0100

    aoc 2023 in kotlin 4

---
+++ 2023_10_03_advent_of_code_in_kotlin/AdventOfCode/src/main/kotlin/adventOfCode2023/day04_cards.kt
@@ -0,0 +1,44 @@
+package adventOfCode2023.day04_cards
+
+import common.eq
+
+data class Card(val winning: Set<Int>, val numbers: Set<Int>)
+
+fun parseNumbers(text: String) =
+    text.splitToSequence(" ").mapNotNull { if (it.isEmpty()) null else it.toInt() }.toSet()
+
+fun parseLine(line: String) =
+    line.substringAfter(":").split("|").let { (winning, numbers) -> Card(parseNumbers(winning), parseNumbers(numbers)) }
+
+fun loadData(input: String) = input.lineSequence().map(::parseLine)
+
+fun calcPoints(n: Int): Int = if (n > 1) 2 * calcPoints(n - 1) else n
+
+fun countWinningNumbers(card: Card) = card.winning.intersect(card.numbers).count()
+
+fun puzzle1(input: String) = loadData(input).sumOf { calcPoints(countWinningNumbers(it)) }.toString()
+
+fun puzzle2(input: String): String {
+    val cards = loadData(input).toList()
+    val amountsOfCards = MutableList(cards.size) { 1 }
+
+    for ((index, card) in cards.withIndex()) {
+        val result = countWinningNumbers(card)
+        val amountOfCards = amountsOfCards[index]
+
+        for (i in (index + 1)..<(index + 1 + result).coerceAtMost(cards.count())) {
+            amountsOfCards[i] += amountOfCards
+        }
+    }
+
+    return amountsOfCards.sum().toString()
+}
+
+
+fun tests() {
+    calcPoints(0) eq 0
+    calcPoints(1) eq 1
+    calcPoints(2) eq 2
+    calcPoints(3) eq 4
+    calcPoints(4) eq 8
+}
