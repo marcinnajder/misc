8d69e5aef03edf0f54c93a14b4c56d011f2a7f0e
marcin najder <marcin.najder@gmail.com>
2023-12-25 18:16:37 +0100

    aoc 2023 in kotlin 15

---
+++ 2023_10_03_advent_of_code_in_kotlin/AdventOfCode/src/main/kotlin/adventOfCode2023/day15_lens.kt
@@ -0,0 +1,38 @@
+package adventOfCode2023.day15_lens
+
+import common.eq
+
+fun loadData(input: String) =
+    input.split(",")
+
+fun decode(code: String) =
+    code.fold(0) { sum, c -> ((sum + c.code) * 17) % 256 }
+
+fun puzzle1(input: String) =
+    loadData(input).sumOf(::decode).toString()
+
+fun parseCode(code: String) =
+    when {
+        code.endsWith("-") -> Pair(code.substringBefore("-"), null)
+        else -> code.split("=").let { (label, len) -> Pair(label, len.toInt()) }
+    }
+
+fun puzzle2(input: String) =
+    loadData(input).asSequence().map(::parseCode) // LinkedHashMap<K, V> preserves the order of insertion
+        .fold(MutableList(256) { linkedMapOf<String, Int>() }) { boxes, (label, length) ->
+            boxes.apply {
+                // remove entry when 'length==null', add or update entry otherwise
+                get(decode(label)).compute(label) { _, _ -> length }
+            }
+        }.asSequence().flatMapIndexed { boxId, lens ->
+            lens.asSequence().mapIndexed { slotId, (_, length) -> (boxId + 1) * (slotId + 1) * length }
+        }.sum().toString()
+
+
+fun tests() {
+    parseCode("rn=1") eq Pair("rn", 1)
+    parseCode("cm-") eq Pair("cm", null)
+    parseCode("pc=4") eq Pair("pc", 4)
+}
+
+
