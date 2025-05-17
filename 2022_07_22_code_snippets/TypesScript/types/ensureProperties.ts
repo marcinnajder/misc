ce9380399351bb924a5ca6d90ceca57d569f6421
marcin najder <marcin.najder@gmail.com>
2025-05-17 09:19:39 +0200

    update snippets ensurePaths

--- 2022_07_22_code_snippets/TypesScript/types/ensureProperties.ts
+++ 2022_07_22_code_snippets/TypesScript/types/ensureProperties.ts
@@ -1,4 +1,7 @@
-// ** ensure(d)Value, ensure(d)Properties, 
+
+
+// *** '...Value'
+
 
 export function ensureValue<T>(value: T | null | undefined): asserts value is NonNullable<T> {
     if (value === null || typeof value === "undefined") {
@@ -6,47 +9,43 @@ export function ensureValue<T>(value: T | null | undefined): asserts value is No
     }
 }
 
-export function ensuredProperty<T extends object, P extends keyof T>(obj: T, propertyName: P): NonNullable<T[P]> {
-    if (obj[propertyName] === null || typeof obj[propertyName] === "undefined") {
-        throw new Error(`property '${String(propertyName)}' of object '{${Object.keys(obj)}}' is null or undefined`);
-    }
-    return obj[propertyName];
-}
-
-export function ensuredValue<T>(value: T | null | undefined): NonNullable<T> {
+export function requiredValue<T>(value: T | null | undefined): NonNullable<T> {
     ensureValue(value)
     return value;
 }
+
+// *** '...Properties'
+
 export type RequiredProperties<T extends object, P extends keyof T> = Omit<T, P> & Required<Pick<T, P>>;
 
-export function ensureProperties<T extends object, P extends keyof T>(
-    obj: T | RequiredProperties<T, P>, ...propertyNames: P[]): asserts obj is RequiredProperties<T, P> {
+function formatMessage(obj: object, propertyName: string): string {
+    return `property '${propertyName}' of '{${Object.keys(obj)}}' object is null or undefined`
+}
+export function ensureProperties<T extends object, P extends keyof T>(obj: T | null | undefined | RequiredProperties<T, P>, ...propertyNames: P[]): asserts obj is RequiredProperties<T, P> {
+    ensureValue(obj);
     for (const propertyName of propertyNames) {
-        ensuredProperty(obj, propertyName);
+        if (obj[propertyName] === null || typeof obj[propertyName] === "undefined") {
+            throw new Error(formatMessage(obj, String(propertyName)));
+        }
     }
 }
 
-// ** ensurePatientProfile
-
-
-export type SubProperties<T extends UserContext, P extends keyof T> = keyof NonNullable<T[P]>;
-
-export type RequiredSubProperties<T extends UserContext, P extends keyof T, SP extends SubProperties<T, P>> =
-    Omit<T, P> & { [key in P]-?: RequiredProperties<NonNullable<T[key]>, SP>; };
-
-export function ensurePatientProfile<T extends UserContext, PP extends SubProperties<T, "patientProfile">>(user: T | RequiredSubProperties<T, "patientProfile", PP>, ...propertyNames: PP[]): asserts user is RequiredSubProperties<T, "patientProfile", PP> {
-    ensureProperties(user, "patientProfile");
-    // ensureProperties(user.patientProfile!, ...propertyNames);
-    ensureProperties(user.patientProfile!, ...(propertyNames as SubProperties<UserContext, "patientProfile">[]));
-    // <- bez rzutowania leci blad TS, poniewaz tutaj chcemy aby "nawigacja Shift+F12" dziala poprawnie
-    // (to szczegolowo jest opisane wyzej), to nie operujemy na 'UserContext' ale na dowolnym 'T extends UserContext',
-    // a wywolanie dla niego 'SubProperties<T, "patientProfile">' zwraca 'string | number | symbol' :/
+export function requiredProperty<T extends object, P extends keyof T>(obj: T | null | undefined, propertyName: P): NonNullable<T[P]> {
+    ensureValue(obj);
+    if (obj[propertyName] === null || typeof obj[propertyName] === "undefined") {
+        throw new Error(formatMessage(obj, String(propertyName)));
+    }
+    // ensureProperties(obj, propertyName);
+    // <- zamiast tego ifa powinnismy wywolac 'ensureProperties', niestety bez ifa nizej pojawia sie blad TS ->   
+    return obj[propertyName];
 }
 
-// ** ensurePaths
+// *** '...Paths'
 
 // Paths<O> -> dzieło Sebastiana :) zwraca wszystkie mozliwe kombinacje sciezek
+
 export type Paths<O> = PathsAux<O, [], "">[number];
+
 type PathsAux<O, Acc extends string[], Current extends string> =
     O extends object ?
     keyof O extends infer Keys
@@ -74,7 +73,8 @@ export type RequiredPaths<T extends {}, P extends Array<Paths<T>>> =
     ? (RequiredSinglePath<T, Head> & RequiredPaths<T, Rest>) : unknown;
 
 
-export function ensurePaths<T extends object, P extends Array<PathsOrString<T>>>(obj: T | RequiredPaths<T, P>, ...propertyPaths: P): asserts obj is RequiredPaths<T, P> {
+export function ensurePaths<T extends object, P extends Array<PathsOrString<T>>>(obj: T | null | undefined | RequiredPaths<T, P>, ...propertyPaths: P): asserts obj is RequiredPaths<T, P> {
+    ensureValue(obj);
     for (const propertyPath of propertyPaths) {
         let currentObj: any = obj;
         for (const propertyName of propertyPath.split(".")) {
@@ -83,6 +83,43 @@ export function ensurePaths<T extends object, P extends Array<PathsOrString<T>>>
         }
     }
 }
+
+export type RequiredSinglePathValue<T extends {}, P extends PathsOrString<T>> =
+    SplitPath<P> extends [infer Head extends keyof T, infer Tail]
+    ? Tail extends string ? RequiredSinglePathValue<NonNullable<T[Head]>, Tail> : NonNullable<T[Head]>
+    : never;
+
+export function requiredPath<T extends object, P extends PathsOrString<T>>(obj: T | null | undefined, propertyPath: P): RequiredSinglePathValue<T, P> {
+    ensurePaths(obj, propertyPath);
+    return propertyPath.split(".").reduce((o, properyName) => o[properyName], obj as any);
+}
+
+// testy typow
+type Person = { name?: string; age?: number; address?: { city?: string; country?: boolean, no?: number } }
+type Type1 = RequiredProperties<Person, "age" | "name">;
+type Type2 = Paths<Person>
+type Type3 = SplitPath<"person.address.city">
+type Type4 = RequiredSinglePath<Person, "address.city">
+type Type5 = RequiredPaths<Person, ["address.city", "address.country", "name"]>
+type Type6 = RequiredSinglePathValue<Person, "address.no">
+// (null as any as Type5).address
+
+
+export type SubProperties<T extends UserContext, P extends keyof T> = keyof NonNullable<T[P]>;
+
+export type RequiredSubProperties<T extends UserContext, P extends keyof T, SP extends SubProperties<T, P>> =
+    Omit<T, P> & { [key in P]-?: RequiredProperties<NonNullable<T[key]>, SP>; };
+
+export function ensurePatientProfile<T extends UserContext, PP extends SubProperties<T, "patientProfile">>(user: T | null | undefined | RequiredSubProperties<T, "patientProfile", PP>, ...propertyNames: PP[]): asserts user is RequiredSubProperties<T, "patientProfile", PP> {
+    ensureValue(user);
+    ensureProperties(user, "patientProfile");
+    // ensureProperties(user.patientProfile!, ...propertyNames);
+    ensureProperties(user.patientProfile!, ...(propertyNames as SubProperties<UserContext, "patientProfile">[]));
+    // <- bez rzutowania leci blad TS, poniewaz tutaj chcemy aby "nawigacja Shift+F12" dziala poprawnie
+    // (to szczegolowo jest opisane wyzej), to nie operujemy na 'UserContext' ale na dowolnym 'T extends UserContext',
+    // a wywolanie dla niego 'SubProperties<T, "patientProfile">' zwraca 'string | number | symbol' :/
+}
+
 
 // ** samples
 
@@ -116,5 +153,5 @@ function ensurePatientProfileTest(user: UserContext) {
 function ensurePathsTest(user: UserContext) {
     ensurePaths(user, "patientProfile.address.city", "patientProfile.address.street");
 
-    const city1 = user.patientProfile.address.city?.toString(); // string | unedfined
+    const city1 = user.patientProfile.address.city.toString(); // string | unedfined
 }
