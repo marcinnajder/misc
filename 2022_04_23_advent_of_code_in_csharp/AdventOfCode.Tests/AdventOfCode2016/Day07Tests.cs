6098f6bfdeedd402d78db0c99097939eefc172a8
marcin.najder <marcin.najder@gmail.com>
2025-09-28 07:41:51 +0200

    C# AoC2016 day7

---
+++ 2022_04_23_advent_of_code_in_csharp/AdventOfCode.Tests/AdventOfCode2016/Day07Tests.cs
@@ -0,0 +1,24 @@
+using AdventOfCode.AdventOfCode2016.Day7;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.AdventOfCode2015.Tests;
+
+[TestClass]
+public class Day07Tests
+{
+    [TestMethod]
+    public void IsAbbaTest()
+    {
+        Assert.IsTrue(Day7.IsAbba("abba"));
+        Assert.IsTrue(Day7.IsAbba("ioxxoj"));
+        Assert.IsFalse(Day7.IsAbba("aaaa"));
+    }
+
+    [TestMethod]
+    public void FindAllAbaTest()
+    {
+        CollectionAssert.AreEqual(new[] { ('a', 'b', 'a') }, Day7.FindAllAba("aba").ToArray());
+        CollectionAssert.AreEqual(new[] { ('z', 'a', 'z'), ('z', 'b', 'z') }, Day7.FindAllAba("zazbz").ToArray());
+        CollectionAssert.AreEqual(new (char, char, char)[] { }, Day7.FindAllAba("aaa").ToArray());
+    }
+}
---
+++ 2022_04_23_advent_of_code_in_csharp/AdventOfCode/AdventOfCode2016/Day07.cs
@@ -0,0 +1,77 @@
+
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+
+namespace AdventOfCode.AdventOfCode2016.Day7;
+
+public record IP(string[] Inside, string[] Outside);
+
+public static class Day7
+{
+    public static IEnumerable<IP> LoadData(string input)
+    {
+        return input
+            .Split(Environment.NewLine, StringSplitOptions.TrimEntries)
+            .Select(line =>
+            {
+                var parts = line.Split('[', ']');
+                var inside = new string[(parts.Length + 1) / 2];
+                var outside = new string[parts.Length - inside.Length];
+                for (int i = 0; i < parts.Length; i++)
+                {
+                    (i % 2 == 0 ? inside : outside)[i / 2] = parts[i];
+                }
+                return new IP(inside, outside);
+            });
+    }
+
+
+    public static bool IsAbba(string text)
+    {
+        for (int i = 1; i < text.Length - 2; i++)
+        {
+            if (text[i] == text[i + 1] && text[i] != text[i - 1] && text[i - 1] == text[i + 2])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static IEnumerable<(char, char, char)> FindAllAba(string text)
+    {
+        for (int i = 0; i < text.Length - 2; i++)
+        {
+            if (text[i] != text[i + 1] && text[i] == text[i + 2])
+            {
+                yield return (text[i], text[i + 1], text[i + 2]);
+            }
+        }
+    }
+
+    public static string Puzzle1(string input)
+    {
+        var ips = LoadData(input);
+        return ips.Count(ip => ip.Inside.Any(IsAbba) && !ip.Outside.Any(IsAbba)).ToString();
+    }
+
+    public static string Puzzle2(string input)
+    {
+        var ips = LoadData(input);
+        return ips.Count(ip =>
+        {
+            foreach (var inside in ip.Inside)
+            {
+                foreach (var (a, b, _) in FindAllAba(inside))
+                {
+                    var search = string.Concat(b, a, b);
+                    if (ip.Outside.Any(outside => outside.Contains(search)))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }).ToString();
+    }
+}
